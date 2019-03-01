7a5f0f988103af6a125a88d7a97904655d1ab7e0
lauracb <lauracadavidb@gmail.com>
2019-03-01 09:29:22 -0500

    Pagination comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @comments = @post.comments.order(created_at: :desc).paginate(page: params[:page], per_page: 2)
+    @comments = @post.comments.order(created_at: :desc).paginate(page: params[:page], per_page: 4)
     @user = current_user
   end
 
